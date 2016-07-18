2e25e2765cbb885d44db10efd7c45fd340cd01f0
aleeex <akang2@gmail.com>
2016-07-18 09:58:56 -0600

    adding our ui routes

--- angularStuff/routes.js
+++ angularStuff/routes.js
@@ -1,20 +1,21 @@
 angular.module('routerApp')
     .config(function($stateProvider, $urlRouterProvider) {
 
-        //This is a catch all for our routes
-
+        //This is a catch all for our routes. "If you can't find the route, send back to home".
+        //Kinda' like a global 404
         $urlRouterProvider.otherwise("/");
 
 //       We need to configure our routes here
 
+        //in the code below, we're adding a .state property to the stateProvider
+        $stateProvider
+          .state('home', {
+            url: '/',
+            templateUrl: 'views/home.html'
+          })
+          .state('contact', {
+            url:'/contact',
+            templateUrl: 'views/contact.html'
+          });
 
 });
-
-
-
-
-
-
-
-
-
